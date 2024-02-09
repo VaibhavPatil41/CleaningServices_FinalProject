780d9617efeba8025d9148ec1fc5a7a9e2133993
VaibhavPatil41 <154294456+VaibhavPatil41@users.noreply.github.com>
2024-02-09 17:10:24 +0530

    merge

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 import Login from './Component/Login';
 
 import RegiStration from './Component/Register';
+import Order from './Component/Order';
 
 function App() {
   return (
     <div className="App">
-      <RegiStration/>
+      {/* <RegiStration/> */}
+      <Order/>
     </div>
     
     
