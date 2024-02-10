bcafdb720ee5c91a6f530a15a2f973e6c05e4e7e
VaibhavPatil41 <154294456+VaibhavPatil41@users.noreply.github.com>
2024-02-10 09:57:10 +0530

    Order

--- src/Component/Order.js
+++ src/Component/Order.js
@@ -1,51 +1,29 @@
 export default function Order(){
 
     return (
+      <section class="container-fluid mt-2" >
         <div>
-            <form class="row g-3">
-  <div class="col-md-3">
-    <label for="inputEmail4" class="form-label">Email</label>
-    <input type="email" class="form-control" id="inputEmail4"/>
+          <h1>Enter Order details</h1>
+        </div>
+        <hr size="5"></hr>
+     <form class="row g-3 mt-2">
+  <div class="col-md-5">
+    <label for="inputEmail4" class="form-label">Date</label>
+    <input type="date" class="form-control" id="inputEmail4"/>
   </div>
-  <div class="col-md-6">
-    <label for="inputPassword4" class="form-label">Password</label>
-    <input type="password" class="form-control" id="inputPassword4"/>
-  </div>
-  <div class="col-12">
+  <div class="col-8">
     <label for="inputAddress" class="form-label">Address</label>
-    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St"/>
+    <input type="text" class="form-control" id="inputAddress" placeholder="house no,street name,area"/>
   </div>
+  <div class="col-8">
+    <label for="inputAddress" class="form-label">Service Name</label>
+    <input type="text" class="form-control" id="inputAddress" placeholder="Service details" disabled/>
+  </div>
+
   <div class="col-12">
-    <label for="inputAddress2" class="form-label">Address 2</label>
-    <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor"/>
-  </div>
-  <div class="col-md-6">
-    <label for="inputCity" class="form-label">City</label>
-    <input type="text" class="form-control" id="inputCity"/>
-  </div>
-  <div class="col-md-4">
-    <label for="inputState" class="form-label">State</label>
-    <select id="inputState" class="form-select">
-      <option selected>Choose...</option>
-      <option>...</option>
-    </select>
-  </div>
-  <div class="col-md-2">
-    <label for="inputZip" class="form-label">Zip</label>
-    <input type="text" class="form-control" id="inputZip"/>
-  </div>
-  <div class="col-12">
-    <div class="form-check">
-      <input class="form-check-input" type="checkbox" id="gridCheck"/>
-      <label class="form-check-label" for="gridCheck">
-        Check me out
-      </label>
-    </div>
-  </div>
-  <div class="col-12">
-    <button type="submit" class="btn btn-primary">Sign in</button>
+    <button type="submit" class="btn btn-outline-primary" disabled>Book</button>
   </div>
 </form>
-        </div>
+    </section>
     )
 }
---
+++ src/Component/OrderStatus.js
@@ -0,0 +1,9 @@
+export default function OrderStatus(){
+    return (
+        <section>
+            <table class="table table-striped table-hover">
+  ...
+</table>
+        </section>
+    )
+}
