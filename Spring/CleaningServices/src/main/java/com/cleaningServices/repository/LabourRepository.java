e86a83056bb53ce42ff0b59c5691adf300e2c3c9
Sangramek <146346773+Sangramek@users.noreply.github.com>
2024-02-21 19:38:19 +0530

    Repo Files Added

--- Spring/CleaningServices/src/main/java/com/cleaningServices/repository/LabourRepository.java
+++ Spring/CleaningServices/src/main/java/com/cleaningServices/repository/LabourRepository.java
@@ -4,11 +4,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.cleaningServices.entities.Labour;
 import com.cleaningServices.entities.ServiceProvider;
+
+import jakarta.transaction.Transactional;
 
 
 @Repository
@@ -18,13 +21,43 @@ public interface LabourRepository extends JpaRepository<Labour, Integer>{
 	public List<Labour> findBySpId(ServiceProvider sp_id);
 	
 	
+	@Query("select l from Labour l where sp_id = :sp_id and l.status != 2")
+	public List<Labour> getLabourbySP(ServiceProvider sp_id);
+	
+	
+	@Modifying
+	@Transactional
+	@Query("UPDATE Labour L SET L.status = 2 WHERE L.labour_id = :id")
+	public void rejectLabour(int id);
+	
+	@Modifying
+	@Transactional
+	@Query("UPDATE Labour SET contactno = :contactno , name=:name ,pan_no=:pan_no WHERE labour_id = :id")
+	public void updateLabour(int id,String name,String contactno,String pan_no);
+	
+	@Query(value="select * from labour where sp_id=:sp_id",nativeQuery = true)
+	public List<Labour> findAll(int sp_id);
+	
+	///////////////////////
+	public Labour findByname(String name);
+	
+	@Query(value="select * from labour where sp_id=:sp_id and status=0",nativeQuery = true)
+	public List<Labour> EmptyAll(int sp_id);
+	
+	
+	//maintain status of labour 0 => 1
+	@Modifying
+	@Transactional
+	@Query("UPDATE Labour SET status = 1  WHERE labour_id = :lid")
+	public void changeStatus(int lid);
+
+	@Query(value="select * from labour where labour_id=:labour_id",nativeQuery=true)
+	public Labour findLabourById(int labour_id);
+	
+	
 	}
 
-//	@Query(value="select * from labour  where sp_id =:spid", nativeQuery = true)
-//	Labour getLabourBySpId(int spid);
 
-//	@Query("select l from labour l where sp_id =:sp")
-//	Labour getLabourById(ServiceProvider sp);
 
 	
 	
--- Spring/CleaningServices/src/main/java/com/cleaningServices/repository/Login_repo.java
+++ Spring/CleaningServices/src/main/java/com/cleaningServices/repository/Login_repo.java
@@ -1,6 +1,7 @@
 package com.cleaningServices.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -10,6 +11,10 @@ import com.cleaningServices.entities.Login;
 public interface Login_repo extends JpaRepository<Login,Integer>{
 	@Query(value = "select * from login where username like :uname",nativeQuery = true)
 	public Login getUserByUsername(String uname);
+	
+	@Modifying
+	@Query(value="update login l set l.username=:username,l.password=:password WHERE loginid=:lid",nativeQuery=true)
+	public int updateLogin(String username, String password,int lid);
 
 	/*@Query(value = "select id from login where username=:uname and password=:pass",nativeQuery = true)
 	public int getUserByUsernamepass(String uname,String pass);
