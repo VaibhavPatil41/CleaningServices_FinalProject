88a264c0528b5da384e33bc5af7f8eab765a6541
maheshpatil-100 <maheshpatil3717@gmail.com>
2024-02-21 19:47:26 +0530

    App.js File added

--- React/src/App.js
+++ React/src/App.js
@@ -4,7 +4,7 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Login from './Component/Login';
 import Order from './Component/Order';
 import OrderStatus from './Component/OrderStatus';
-import Services from './Component/Services';
+//import Services from './Component/Services';
 import Feedback from './Component/Feedback';
 import { Logout } from './Component/Logout';
 import { useSelector } from "react-redux"
@@ -15,11 +15,29 @@ import Home from './WebPages/Home';
 import CustWelcome from './CustomerComponents/CustHome';
 import AdminWelcome from './AdminComponents/AdminHome';
 import RegiStrationS from './SPComponents/ServiceProviderRegister';
-import RegiStration1 from './CustomerComponents/CustomerRegister';
 import ApproveReq from './AdminComponents/approveRequest';
 import AddLabour from './SPComponents/AddLabour';
-import AddService from './SPComponents/AddService';
-import UsingEffectComp from './AdminComponents/UsingEffectComp';
+import AddService from './SPComponents/AddServiceVaibhav';/////////////
+import UsingEffectComp from './Component/UsingEffectComp';
+import LandingPage from './landingPages/LandingPage';
+import Header from './landingPages/Header';
+import UserLoginSMT from './landingPages/UserLogin';
+import Footer from './landingPages/Footer';
+import AboutUs from './landingPages/AboutUs';
+import ContactUs from './landingPages/ContactUs';
+import RegiStration1 from './CustomerComponents/CustomerRegister';
+import AdminDash from './AdminComponents/AdminDashboard';
+import CustomerDash from './CustomerComponents/CustomerDashboard';
+import SPDash from './SPComponents/SPDashboard';
+import ViewLabour from './SPComponents/ViewLabour';
+import EditLabour from './SPComponents/EditLabour';
+import EditProfileCust from './CustomerComponents/EditProfileCust';
+import EditSP from './SPComponents/EditSP';
+import ConfirmBooking from './CustomerComponents/ConfirmBookingVaibhav';
+import ViewServicesSP from './SPComponents/viewServicesVarad';
+import CustDiplayService from './CustomerComponents/CustomerService';
+import SpOrders from './SPComponents/SpOrders';
+import CustOrders from './CustomerComponents/CustOrders';
 
 
 
@@ -68,20 +86,24 @@ function App() {
         </ul>
       </div> */}
       <Routes>
+        <Route path='/home' element={<Home/>} />
         <Route path="/login" element={<Login/>  }/> 
         {/* <Route path="/reg" element={<RegiStration/> }/>  */}
-        <Route path="/services" element={<Services/> }/> 
+        {/* <Route path="/services" element={<Services/> }/>  */}
         <Route path="/feedback" element={<Feedback/> }/>
 
         <Route path="/order" element={<Order/>} />    
         <Route path="/orderStatus" element={<OrderStatus/>} />    
         <Route path="/logout" element={<Logout/>} />   
 
-        <Route path='/' element={<Home/>} />
-        <Route path='/spWelcome' element={<SPWelcome/> } />
+       
+        {/* <Route path='/spWelcome' element={<SPWelcome/> } /> */}
+        <Route path='/spWelcome' element={<SPDash/> } />
         
-        <Route path='/adminWelcome' element={<AdminWelcome/>} />       
-        <Route path='/custWelcome' element={<CustWelcome/>} />
+        {/* <Route path='/adminWelcome' element={<AdminWelcome/>} />     */}
+        <Route path='/adminWelcome' element={<AdminDash/>} />     
+        {/* <Route path='/custWelcome' element={<CustWelcome/>} /> */}
+        <Route path='/custWelcome' element={<CustomerDash/>} />
         <Route path='/welcome' element={<Welcome/> } />
         <Route path='/editProfile' element={<EditProfile/> } />
 
@@ -90,10 +112,38 @@ function App() {
         <Route path="/approve" element={<ApproveReq/> }/>
         <Route path="/addlabour" element={<AddLabour/> }/>
         <Route path="/addservice" element={<AddService/>}/>
+
+
         <Route path="/effect" element={<UsingEffectComp/>}/>
+
+
+
+        <Route path="*" element={<LandingPage/>}/>
+        {/* <Route path="/" element={<LandingPage/>}/> */}
+        <Route path="/header" element={<Header/>}/>
+        <Route path="/ulogin" element={<UserLoginSMT/>}/>
+        <Route path="/aboutus" element={<AboutUs/>}/>
+        <Route path="/ContactUs" element={<ContactUs/>}/>
+        <Route path="/viewLabour" element={<ViewLabour/>}/>
+        <Route path="/editLabour" element={<EditLabour/>}/>
+        <Route path="/editCust" element={<EditProfileCust/>}/>
+        <Route path="/editSP" element={<EditSP/>}/>
+
+
+
+        <Route path="/viewservicessp" element={<ViewServicesSP/>}/>
+        {/* <Route path="/confirmBooking" element={<ConfirmBooking/>}/> */}
+        <Route path="/customerService" element={<CustDiplayService/>}/>
+        
+        <Route path="/custOrder" element={<CustOrders/>}/>
+        <Route path="/spOrder" element={<SpOrders/>}/>
+        <Route path="/confirmBook" element={<ConfirmBooking/>}/>
+       
+
         
 
-       {/* <LandingPage/> */}
+
+       
         
        
         
